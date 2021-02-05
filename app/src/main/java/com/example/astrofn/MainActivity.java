277d467afba72973a42ac99439e076dd1daf0972
Nawres99 <nawreshamdi99@gmail.com>
2021-02-05 02:18:16 -1100

    fragment mch kemlin

--- app/src/main/java/com/example/astrofn/MainActivity.java
+++ app/src/main/java/com/example/astrofn/MainActivity.java
@@ -1,39 +1,35 @@
 package com.example.astrofn;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.astro.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private Toolbar toolbar;
-    RecyclerView recyclerView;
-    RecyclerView.Adapter programAdapter;
-    RecyclerView.LayoutManager layoutManager;
-    String[] planets = {"Earth","Mars","Neptune","Uranus","Mercury","Saturn","Jupiter"};
-    String[] descriptions = {"Earth desc","Mars desc","Neptune desc","Uranus desc","Mercury desc","Saturn desc","Jupiter desc"};
-    int[] images ={R.drawable.earth , R.drawable.mars,R.drawable.neptune,R.drawable.uranus,R.drawable.mercury , R.drawable.saturn,R.drawable.jupiter};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar=findViewById(R.id.action_bar);
-        setSupportActionBar(toolbar);
-        recyclerView=findViewById(R.id.recycler);
-        recyclerView.setHasFixedSize(true);
-        layoutManager=new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        programAdapter = new ProgramAdapter(this,planets,descriptions,images);
-        recyclerView.setAdapter(programAdapter);
-
-
-
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        Set<Integer> topLevelDestinations = new HashSet<>();
+        topLevelDestinations.add(R.id.listePlanet);
+        topLevelDestinations.add(R.id.loginActivity);
+        topLevelDestinations.add(R.id.quizz);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(topLevelDestinations).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     }
 }
